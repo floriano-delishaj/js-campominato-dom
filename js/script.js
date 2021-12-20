3c2d02378185aabcfa23ff8e938bee0013f0b211
floriano-delishaj <delishaj.floriano@live.it>
2021-12-20 15:42:27 +0100

    beta

---
+++ js/script.js
@@ -0,0 +1,73 @@
+function randomNumber (minNum, maxNum) {
+    const result = Math.floor(Math.random() * ((maxNum + 1) - minNum)) + minNum;
+    return result;
+}
+
+function getRandomNumber (k) {
+
+    const array = [];
+
+    while (array.length < 16) {
+        const num = randomNumber (1, k);
+        if (!array.includes(num)) {
+            array.push(num)
+        }
+    }
+
+    console.log(array);
+    return array;
+}
+
+function createBox (output, x) {
+    const newBox = document.createElement("div");
+    newBox.className = 'box';
+    newBox.innerHTML = x;
+    output.append(newBox);
+    newBox.addEventListener('click', function () {
+        if (array.includes(x)) {
+            console.log('perso')
+        } else {
+            console.log('vinto');
+        }
+    })
+}
+
+//mettere l'addEventListener del newBox all'interno del for così ogni volta che creare un box compara il numero dell'array con i numeri random a quello della i
+
+function createGrid (max) {
+    for (let i = 1; i <= max; i++) {
+
+        createBox(containerOutput, i);
+    }
+}
+
+const container = document.querySelector('.container');
+const containerOutput = document.querySelector('.container-grid');
+const selectionOne = document.getElementById('selection-1');
+const selectionTwo = document.getElementById('selection-2');
+const selectionThree = document.getElementById('selection-3');
+
+// alert('Premi un pulsante per iniziare a giocare');
+
+selectionOne.addEventListener('click', function () {
+    containerOutput.innerHTML = ''; //ripulire la griglia
+    getRandomNumber(100);
+    createGrid(100);
+    container.classList.remove('small', 'medium');
+    container.classList.add('big');
+})
+selectionTwo.addEventListener('click', function () {
+    containerOutput.innerHTML = '';
+    getRandomNumber(81);
+    createGrid(81);
+    container.classList.remove('big', 'small');
+    container.classList.add('medium');
+})
+
+selectionThree.addEventListener('click', function () {
+    containerOutput.innerHTML = '';
+    getRandomNumber(49)
+    createGrid(49);
+    container.classList.remove('big', 'medium');
+    container.classList.add('small');;
+})
